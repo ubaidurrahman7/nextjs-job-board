767045197b1d2c114a695100b17af19621b14709
Ubaid ur Rahman <ubaidurr888999@gmail.com>
2024-01-28 00:35:30 +0500

    updated the project by adding sidebar filter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import prisma from "@/lib/prisma";
 import JobListItem from "@/components/jobListItem";
+import JobFilterSidebar from "@/components/JobFilterSidebar";
 
 export default async function Home() {
   const jobs = await prisma.job.findMany({
@@ -9,10 +10,21 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main>
-      {jobs.map((job) => (
-        <JobListItem job={job} key={job.id} />
-      ))}
+    <main className="m-auto my-10 max-w-5xl space-y-10 px-3">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Developer jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream job</p>
+      </div>
+      <section className="flex flex-col gap-4 md:flex-row">
+        <JobFilterSidebar />
+        <div className="grow space-y-4">
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
